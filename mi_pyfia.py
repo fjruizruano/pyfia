e143063dabcf8862c9f0a2bf6aff44c48c09953a
fjruizruano <fjruizruano@2f7f88f0-4157-11de-b5d3-5d1980107c5c>
2010-08-02 14:32:50 +0000

    https://pyfia.googlecode.com/svn/

--- mi_pyfia.py
+++ mi_pyfia.py
@@ -2,63 +2,70 @@
 import math
 import fnmatch
 import os
-from PIL import Image
+from PIL import Image # instalar desde repositorios
+import numpy # instalar desde repositorios
+import mahotas # ver wiki para instalar
 
 #
 # La actual versión del algoritmo pyFIA para cada imagen/núcleo en una determinada ruta:
 # 1. Lee la matriz de píxeles del canal verde de una imagen en color o el único canal si está en escala de grises.
-# 2. Estima la media del fondo para definir un umbral.
-# 3. A los píxeles cuya intensidad está por debajo del umbral se les calcula la densidad óptica.
-# 4. La densidad óptica integrada es la suma de densidades ópticas para cada núcleo.
+# 2. Estima el umbral por el método de Otsu usando las librerías de mahotas.
+# 3. Estima la media del fondo (pixels cuyo valor es superior al umbral).
+# 4. A los píxeles cuya intensidad está por debajo del umbral se les calcula la densidad óptica.
+# 5. La densidad óptica integrada es la suma de densidades ópticas para cada núcleo.
+# La media de foto y la densidad óptica de cada píxel están redondeadas al segundo decimal según Rasch 2006.
 #
 
 def algoritmo(ruta, foto):
 
-	def calcular_iod(pixmap, lower_limit, upper_limit, gap_limit):
+	def calcular_iod(pixmap):
 		
 		iod = 0 # densidad óptica integrada de cada núcleo
 		pixels_fondo = 0 # número de píxels del fondo
 		suma_fondo = 0 # suma de valores de los píxeles del fondo
 		media_fondo = 0 # valor medio de los píxeles del fondo
-		umbral = 0 # valor de píxel por debajo del cual se considera núcleo
-		
+
 		for pixels in pixmap:
-			if lower_limit < int(pixels) < upper_limit: 
+			if pixels > umbral: 
 				pixels_fondo += 1
 				suma_fondo += int(pixels)
 
 		media_fondo = 1.0 * suma_fondo/pixels_fondo # calcula la media del fondo de la imagen
-		umbral = -math.log10((media_fondo-gap_limit)/media_fondo) # define el umbral entre núcleo y fondo
 
 		for pixels in pixmap:
-			if int(pixels) != 0:  
-				logaritmo = -math.log10(int(pixels)/media_fondo)  # calcula densidad óptica individual de cada píxel
-			else:
-				logaritmo = 0 # para que no dé error si el valor de píxel es 0
 
-			if logaritmo > umbral:
-				iod += logaritmo # calcula densidad óptica integrada del núcleo
+			if int(pixels) <= umbral and int(pixels) != 0: # pixels inferiores al umbral y distintos de 0
+				logaritmo = -math.log10(int(pixels)/round(media_fondo,2))  # calcula densidad óptica individual de cada píxel
+				iod += round(logaritmo,2) # calcula densidad óptica integrada del núcleo
 				
-		return iod, media_fondo
+		return iod, umbral
 
 	direccion = ruta +"/" + foto # Ruta completa a la imagen
 
 	if fnmatch.fnmatch(foto, '*.tif'):
-		imagen = Image.open(str(direccion))
-		pixmap = list(imagen.getdata(1))
-		(iod, media_fondo) = calcular_iod(pixmap, 180, 210, 10)
-	elif fnmatch.fnmatch(foto, '*.txt') :
-		imagen = open(str(direccion), 'r')
-		leer = imagen.read()
-		pixmap = leer.split()
-		(iod, media_fondo) = calcular_iod(pixmap, 30000, 50000, 640)
+#		pixmap = scipy.misc.pilutil.imread(str(direccion)) # abrir imagen con scipy
+		imagen = Image.open(str(direccion)) # abrir imagen tif
+		if imagen.mode == "L":
+			pixmap = list(imagen.getdata())  # realizar una lista con los valores del canal verde
+		elif imagen.mode == "RGB" or "RGBA":
+			pixmap = list(imagen.getdata(1))  # realizar una lista con los valores del canal
+		dna = numpy.array(pixmap, dtype='uintc') # de lista a array numpy
+		umbral = mahotas.thresholding.otsu(dna) # umbral por el metodo de otsu
+		(iod, umbral) = calcular_iod(pixmap) #aplicamos el algoritmo
+	elif fnmatch.fnmatch(foto, '*.txt'):
+		imagen = open(str(direccion), 'r') # abrir fichero de texto
+		leer = imagen.read() # leer
+		pixmap = leer.split() # lista con los valores del fichero
+		dna = numpy.array(pixmap, dtype='uintc') # de lista a array numpy
+		umbral = mahotas.thresholding.otsu(dna) # umbral por el metodo de otsu
+		(iod, umbral) = calcular_iod(pixmap) # aplicamos el algoritmo
 	
-	return iod, media_fondo
+	return iod, umbral
 
 def abre_salida(ruta):
 	f_salida = ruta + "/output"
 	salida = open(f_salida ,"w")
-	salida.write("%s \t %s \t \t %s" % ("imagen", "IOD", "fondo" "\n"))
+	salida.write("%s \t %s \t  %s" % ("imagen", "IOD", "umbral" "\n"))
 	salida.write("-" * 40)
 	return salida
 
@@ -87,15 +94,14 @@ def aplicar_algoritmo(ruta):
 					primer_fichero = False					
 				
 				print "Analizando " + current_file
-				(iod, media_fondo) = algoritmo(root, current_file)
+				(iod, umbral) = algoritmo(root, current_file)
 				
 				iod_str = str(iod).replace(".", ",")
-				media_fondo_str = str(media_fondo).replace(".", ",")
-				salida.write("\n%s \t %s \t %s" % (current_file, iod_str, media_fondo_str))
+#				media_fondo_str = str(media_fondo).replace(".", ",")
+				salida.write("\n%s \t %s \t %s" % (current_file, iod_str, umbral))
 		
 		if primer_fichero is False:
 			salida.close()
-
 
 print "\nThis is the Funny Program for Feulgen Image Analysis Densitometry"
 url = str(raw_input("\n>>>Introduzca ruta de las imagenes:"))
